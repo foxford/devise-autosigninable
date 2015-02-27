94697f6cec0eb3cf57100326e0ad3f2273a64e36
Dimko <deemox@gmail.com>
2015-02-27 15:55:36 +0300

    fixed deprecation warning about the route where `to` is a controller

--- lib/devise/autosigninable/routes.rb
+++ lib/devise/autosigninable/routes.rb
@@ -4,7 +4,7 @@ ActionDispatch::Routing::Mapper.class_eval do
   # Setup routes for +AutosigninController+.
   def devise_autosignin(mapping, controllers)
     get "/:#{mapping.name}_id/autosignin/:autosignin_token",
-      :to => controllers[:autosignin],
+      :controller => controllers[:autosignin],
       :action => 'create',
       :as => :autosignin
   end
